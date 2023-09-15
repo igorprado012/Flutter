3630fff206f264d36b2b7959caf1a77b0f7f2e91
Igor Prado <igor.uba1997@gmail.com>
2023-09-15 19:32:23 -0300

    Update main.dart

--- Flut/first_app/lib/main.dart
+++ Flut/first_app/lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: Center(child: Text('oi')))));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 239, 239, 239),
+        body: Center(
+          child: Text('HERMES S.E'),
+        ),
+      ),
+    ),
+  );
 }
