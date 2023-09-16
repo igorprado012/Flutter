a73ae6473f9160f2179875b9235b831fd3108dd6
Igor Prado <igor.uba1997@gmail.com>
2023-09-16 18:11:24 -0300

    letras hermes

--- Flut/first_app/lib/main.dart
+++ Flut/first_app/lib/main.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 239, 239, 239),
-        body: Center(
-          child: Text('HERMES S.E'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(98, 219, 232, 0.992),
+                Color.fromRGBO(225, 239, 239, 0.593)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'HERMES S.E',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 40,
+              ),
+            ),
+          ),
         ),
       ),
     ),
