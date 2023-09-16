2efc106ac654eed9267e4640548ec731907cacf5
Igor Prado <igor.uba1997@gmail.com>
2023-09-16 19:06:52 -0300

    arrumado ate o gradiente da parte de cima

--- Flut/first_app/lib/main.dart
+++ Flut/first_app/lib/main.dart
@@ -8,19 +8,22 @@ void main() {
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color.fromRGBO(98, 219, 232, 0.992),
-                Color.fromRGBO(225, 239, 239, 0.593)
+                Color.fromRGBO(74, 229, 229, 0.992),
+                Color.fromRGBO(225, 239, 239, 0.593),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomLeft,
             ),
           ),
           child: const Center(
+            heightFactor: 3,
             child: Text(
               'HERMES S.E',
+              textDirection: TextDirection.ltr,
+              // TextDirection.ltr significa que o texto será lido da esquerda para direita.
               style: TextStyle(
                 color: Colors.black87,
-                fontSize: 40,
+                fontSize: 64,
               ),
             ),
           ),
