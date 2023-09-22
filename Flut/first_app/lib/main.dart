25e2298cec6846d81ccae445d5f681933b712a80
Igor Prado <igor.uba1997@gmail.com>
2023-09-22 15:45:47 -0300

    gradiente e nome com super kay

--- Flut/first_app/lib/main.dart
+++ Flut/first_app/lib/main.dart
@@ -2,45 +2,42 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Gradiente_Container(),
+        body: Nome_Container(),
       ),
     ),
   );
 }
 
-class Gradiente_Container extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(74, 229, 229, 0.992),
-            Color.fromRGBO(225, 239, 239, 0.593),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomLeft,
-        ),
-      ),
-    );
-  }
-}
-
+// ignore: camel_case_types
 class Nome_Container extends StatelessWidget {
+  const Nome_Container({super.key});
+
   @override
   Widget build(context) {
-    return Container(
-      child: const Center(
-        heightFactor: 3,
-        child: Text(
-          'HERMES S.E',
-          textDirection: TextDirection.ltr,
-          // TextDirection.ltr significa que o texto será lido da esquerda para direita.
-          style: TextStyle(
-            color: Colors.black87,
-            fontSize: 64,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(74, 229, 229, 0.992),
+              Color.fromRGBO(225, 239, 239, 0.593),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        child: const Center(
+          heightFactor: 3,
+          child: Text(
+            'HERMES S.E',
+            textDirection: TextDirection.ltr,
+            // TextDirection.ltr significa que o texto será lido da esquerda para direita.
+            style: TextStyle(
+              color: Colors.black87,
+              fontSize: 64,
+            ),
           ),
         ),
       ),
