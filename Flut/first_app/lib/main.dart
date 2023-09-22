5ea47dbe18f66dc5fc678d13df97e6ad39f016c4
Igor Prado <igor.uba1997@gmail.com>
2023-09-22 15:21:56 -0300

    nome e gradiente separados em clase.
    ainda em erro ao juntar os dois

--- Flut/first_app/lib/main.dart
+++ Flut/first_app/lib/main.dart
@@ -4,31 +4,46 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(74, 229, 229, 0.992),
-                Color.fromRGBO(225, 239, 239, 0.593),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomLeft,
-            ),
-          ),
-          child: const Center(
-            heightFactor: 3,
-            child: Text(
-              'HERMES S.E',
-              textDirection: TextDirection.ltr,
-              // TextDirection.ltr significa que o texto será lido da esquerda para direita.
-              style: TextStyle(
-                color: Colors.black87,
-                fontSize: 64,
-              ),
-            ),
-          ),
-        ),
+        body: Gradiente_Container(),
       ),
     ),
   );
+}
+
+class Gradiente_Container extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(74, 229, 229, 0.992),
+            Color.fromRGBO(225, 239, 239, 0.593),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomLeft,
+        ),
+      ),
+    );
+  }
+}
+
+class Nome_Container extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      child: const Center(
+        heightFactor: 3,
+        child: Text(
+          'HERMES S.E',
+          textDirection: TextDirection.ltr,
+          // TextDirection.ltr significa que o texto será lido da esquerda para direita.
+          style: TextStyle(
+            color: Colors.black87,
+            fontSize: 64,
+          ),
+        ),
+      ),
+    );
+  }
 }
